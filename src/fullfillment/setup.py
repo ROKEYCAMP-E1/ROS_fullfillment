ecee77f77bbe84c23ca8061914159ba13dc719ce
sy3180 <sy3180@gamil.com>
2024-11-28 20:08:58 +0900

    Update ff_interface and fullfillment package configurations

--- src/fullfillment/setup.py
+++ src/fullfillment/setup.py
@@ -10,13 +10,12 @@ setup(
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
     ],
-    install_requires=['setuptools', 'rclpy', 'opencv-python'],  # 필요한 의존성 추가
+    install_requires=['rclpy', 'opencv-python'],  # 필요한 의존성 추가
     zip_safe=True,
     maintainer='rokey',
     maintainer_email='sy3180@gamil.com',
     description='TODO: Package description',
     license='TODO: License declaration',
-    tests_require=['pytest'],
     entry_points={
         'console_scripts': [
             'UI_node = fullfillment.UI.ui_node:main',
